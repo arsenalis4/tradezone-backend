b994bc985e66be933a1048c519592e8cbe7f40e7
서동재 <hanatoal1234@naver.com>
2025-06-28 19:09:15 +0900

    fix: add jpa auditing

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,10 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 @OpenAPIDefinition(
 	info = @Info(
 		title = "TradeZone Portfolio API",
